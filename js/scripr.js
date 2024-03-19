6c8cdaeeef81f390c3622c146b1e008c551a8e5c
Isaias Carlos <109770805+ICN153@users.noreply.github.com>
2024-03-19 20:59:04 -0300

    Add files via upload

--- js/scripr.js
+++ js/scripr.js
@@ -1,6 +1,54 @@
+
+// Calculando o IMC
+// var -> Criar uma variável, pórem, permite que esta seja "redeclarada" podendo causar BUGS
+// Não permite que a variavel seja "redeclarada" e só funciona dentro do seu escopo
+// const -> semelhante ao let com uma observação: NÃO PODE TER SEU VALOR ALTERADO
+
+//Fórmula do IMC: imc peso / (altura*altura)
+
+
+const peso = document.querySelector('#peso');
+const altura = document.querySelector('#altura');
+const btnCalcular = document.querySelector('#btn-calcular');
+const contentIMC = document.querySelector('.content-imc');
+const resultIMC = document.querySelector('.result-imc');
+
+btnCalcular.addEventListener('click', function(){
+
+    //se peso e altura forem diferentes de vazio
+    // == comparação (igualdade)
+    // != comparação (diferente)
+    // && e
+    // || ou
+    
+   
+if(peso.value != '' && altura.value != ''){
+
+    //Alterando a classe "show"
+    contentIMC.classList.toggle('show');
+    resultIMC.classList.toggle('show');
+
+let valorPeso = parseFloat(peso.value);
+let valorAltura = parseFloat(altura.value);
+let imc = valorPeso / (valorAltura*valorAltura);
+//alert(imc);
+
+const infoResult = document.querySelector('.result-imc h2 span');
+infoResult.innerHTML = imc.toFixed(2);
+
+}
+
+else{
+    alert('Preencha os campos para calcular');
+}
+
+})
+
+
+
 //selecionar os elementos que serão manipulados pelo javascript
-var lgpd = document.querySelector('.lgpd');
-var btnLgpd = document.querySelector('.lgpd button');
+let lgpd = document.querySelector('.lgpd');
+let btnLgpd = document.querySelector('.lgpd button');
 
 
 //console.log(lgpd);
