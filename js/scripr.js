4cf49dc90474d393f3a9bfb64cfa9455a8b58802
Isaias Carlos <109770805+ICN153@users.noreply.github.com>
2024-03-21 20:44:03 -0300

    Add files via upload

--- js/scripr.js
+++ js/scripr.js
@@ -12,63 +12,86 @@ const altura = document.querySelector('#altura');
 const btnCalcular = document.querySelector('#btn-calcular');
 const contentIMC = document.querySelector('.content-imc');
 const resultIMC = document.querySelector('.result-imc');
+const btnResult = document.querySelector('.result-imc button');
 
-btnCalcular.addEventListener('click', function(){
+const infoResult = document.querySelector('.result-imc h2 span'); //IMC
+const infoClass = document.querySelector('.result-imc p span'); //Classificação
+
+function colorIMC(cor){
+    infoClass.style.color = cor;
+    infoResult.style.color = cor;
+}
+
+
+btnCalcular.addEventListener('click', function () {
 
     //se peso e altura forem diferentes de vazio
     // == comparação (igualdade)
     // != comparação (diferente)
     // && e
     // || ou
-    
-   
-if(peso.value != '' && altura.value != ''){
 
-    //Alterando a classe "show"
+
+    if (peso.value != '' && altura.value != '') {
+
+        //Alterando a classe "show"
+        contentIMC.classList.toggle('show');
+        resultIMC.classList.toggle('show');
+
+        let valorPeso = parseFloat(peso.value);
+        let valorAltura = parseFloat(altura.value);
+        let imc = valorPeso / (valorAltura * valorAltura);
+        //alert(imc);
+
+        
+        infoResult.innerHTML = imc.toFixed(2);
+
+        if(imc < 18.5){
+            infoClass.innerHTML = 'Magreza';
+            colorIMC('red');
+        }
+
+        else if(imc >= 18.5 && imc <= 24.9){
+            infoClass.innerHTML = "Normal";
+            colorIMC('green');
+        }
+
+        else if(imc >= 25.0 && imc <= 29.9){
+            infoClass.innerHTML = "Sobrepeso";
+            colorIMC('orange');
+        }
+
+        else if(imc >= 30.0 && imc <= 39.9){
+            infoClass.innerHTML = "Obesidade";
+            colorIMC('red');
+        }
+
+        else{
+            infoClass.innerHTML = "Obesidade Grave"
+            colorIMC('#B10303')
+        }
+
+    }
+
+    else {
+        alert('Preencha os campos para calcular');
+    }
+
+})
+
+
+
+//alert('ok');
+//Ao clicar no botão de refazer o teste
+btnResult.addEventListener('click', function () {
+
     contentIMC.classList.toggle('show');
     resultIMC.classList.toggle('show');
-
-let valorPeso = parseFloat(peso.value);
-let valorAltura = parseFloat(altura.value);
-let imc = valorPeso / (valorAltura*valorAltura);
-//alert(imc);
-
-const infoResult = document.querySelector('.result-imc h2 span');
-infoResult.innerHTML = imc.toFixed(2);
-
-}
-
-else{
-    alert('Preencha os campos para calcular');
-}
+    
+    //Limpando os campos
+    peso.value = '';
+    altura.value = '';
 
 })
 
 
-
-//selecionar os elementos que serão manipulados pelo javascript
-let lgpd = document.querySelector('.lgpd');
-let btnLgpd = document.querySelector('.lgpd button');
-
-
-//console.log(lgpd);
-//console.log(btnLgpd);
-
-//Associando o evento de click ao botão da lgpd
-
-btnLgpd.addEventListener('click',function(){
-    //alert('ok');
-
-    lgpd.style.display = 'none';
-
-    // Além de fechar, vamos guardar uma informação no navegador do usuário
-
-    localStorage.setItem('fechouLgpd','sim');
-
-})
-
-//Se o usuário já fechou a lgpd (tem a informação guardada no storage)
-// se a informação guardada no storage é igual (==) a 'sim'
-if(localStorage.getItem('fechouLgpd') == 'sim'){
-    lgpd.style.display = 'none';
-}
